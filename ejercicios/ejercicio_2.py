151a9d76c95395e6c9380e586b3eb6a77131604d
Uriel <urield1917@gmail.com>
2022-04-28 19:53:04 -0300

    corregido

--- ejercicios/ejercicio_2.py
+++ ejercicios/ejercicio_2.py
@@ -1,17 +1,17 @@
 # Ejercicio 2 - Uriel Diaz
 palabras = []
+numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 salir = 1
 
 print("Ingrese las palabras que desee ordenar\nPara dejar de ingresar, escriba Exit")
 while salir:
-    try:
-        palabras.append(str(input()))
-        for palabra in palabras:
-            if palabra == "exit" or palabra == "Exit":
-                salir = 0
-
-    except ValueError:
-        print("No ingreses caracteres numéricos/especiales!")
+    palabras.append(input())
+    for palabra in palabras:
+        if palabra in numeros:
+            print("No ingreses caracteres numéricos/especiales!")
+            palabras.remove(palabra)
+        elif palabra == "exit" or palabra == "Exit":
+            salir = 0
 
 palabras.remove("exit")
 print("-".join(sorted(palabras)))
